1d553109557604a7f383a92d3c229ad5d34c868d
Igors78 <oleinikovsigors@gmail.com>
2022-09-29 01:35:59 +0300

    Add check cases to main

--- home_work_one/Main.java
+++ home_work_one/Main.java
@@ -4,9 +4,27 @@ import static java.lang.System.out;
 
 public class Main {
 	public static void main(String[] args) {
-		Invoice i01 = new Invoice("#345632", "Spare part", 5, 43);
+		Invoice i01 = new Invoice("#345632", "Spare part", 5, 40);
 		out.println(i01.getPartDescription() + " " + i01.getPartNumber()
 				+ " quantity: " + i01.getQuantity() + " with price of " + i01.getPrice());
 		out.println("Total invoice amount is: " + i01.getInvoiceAmount());
+		i01.setQuantity(-5);
+		out.println(i01.getPartDescription() + " " + i01.getPartNumber()
+				+ " quantity: " + i01.getQuantity() + " with price of " + i01.getPrice());
+		out.println("Total invoice amount is: " + i01.getInvoiceAmount());
+		i01.setQuantity(5);
+		i01.setPrice(-40);
+		out.println(i01.getPartDescription() + " " + i01.getPartNumber()
+				+ " quantity: " + i01.getQuantity() + " with price of " + i01.getPrice());
+		out.println("Total invoice amount is: " + i01.getInvoiceAmount());
+		i01.setPrice(40);
+		out.println(i01.getPartDescription() + " " + i01.getPartNumber()
+				+ " quantity: " + i01.getQuantity() + " with price of " + i01.getPrice());
+		out.println("Total invoice amount is: " + i01.getInvoiceAmount());
+		Invoice i02 = new Invoice("#300002", " Another Spare part", -15, -240);
+		out.println(i02.getPartDescription() + " " + i02.getPartNumber()
+				+ " quantity: " + i02.getQuantity() + " with price of " + i02.getPrice());
+		out.println("Total invoice amount is: " + i02.getInvoiceAmount());
+
 	}
 }
